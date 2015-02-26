5c1f240d2506b7f687d4fc6d513d1da480b66a89
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-02-25 18:38:11 -0800

    Add 'simpler_diceSimulator.rb'.

---
+++ simpler_diceSimulator.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+# This is a simpler version of the Dice Simulator, similar to the original
+# C version you pushed. You know, without all of the extra stuff: argument
+# parsing... the progress bar... etc. It should be pretty easy to understand.
+
+# Constant variables should be all caps.
+DIE1_NUM_SIDES = 6
+DIE2_NUM_SIDES = 6
+SAMPLE_SIZE    = 1000
+
+ARRAY_SIZE = DIE1_NUM_SIDES + DIE2_NUM_SIDES + 1
+
+# Create a new, zero-filled array.
+results = Array.new(ARRAY_SIZE) {0}
+
+# Simulate the dice rolls and sum the results.
+SAMPLE_SIZE.times do |i|
+    die1 = rand(1..DIE1_NUM_SIDES)
+    die2 = rand(1..DIE2_NUM_SIDES)
+
+    total = die1 + die2
+    results[die1 + die2] += 1
+end
+
+# Open the CSV file.
+csv = File.open("data.csv", "w")
+csv.write("value,occurrences\n")
+
+# Print out the results, and write it to the CSV.
+results.each_with_index do |occurrences, value|
+    printf("%2d: %2d\n", value, occurrences)
+    csv.write("#{value},#{occurrences}\n")
+end
+
+# Graph it in R.
+`Rscript ./plot.R`
+File.delete("data.csv")
