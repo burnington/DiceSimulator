f012f21e7f5cfd928f7eee1fd473156b442d0c98
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-02-25 19:26:22 -0800

    Add simple ruby and bash.

--- simple_examples_of_different_languages/dice.rb
+++ simple_examples_of_different_languages/dice.rb
@@ -4,20 +4,20 @@
 # C version you pushed. You know, without all of the extra stuff: argument
 # parsing... the progress bar... etc. It should be pretty easy to understand.
 
-# Constant variables should be all caps.
-DIE1_NUM_SIDES = 6
-DIE2_NUM_SIDES = 6
+# In Ruby, constant variables should be all caps.
+DIE1_SIDES = 6
+DIE2_SIDES = 6
 SAMPLE_SIZE    = 1000
 
-ARRAY_SIZE = DIE1_NUM_SIDES + DIE2_NUM_SIDES + 1
+ARRAY_SIZE = DIE1_SIDES + DIE2_SIDES - 1
 
 # Create a new, zero-filled array.
 results = Array.new(ARRAY_SIZE) {0}
 
 # Simulate the dice rolls and sum the results.
 SAMPLE_SIZE.times do |i|
-    die1 = rand(1..DIE1_NUM_SIDES)
-    die2 = rand(1..DIE2_NUM_SIDES)
+    die1 = rand(DIE1_SIDES)
+    die2 = rand(DIE2_SIDES)
 
     total = die1 + die2
     results[die1 + die2] += 1
@@ -29,10 +29,11 @@ csv.write("value,occurrences\n")
 
 # Print out the results, and write it to the CSV.
 results.each_with_index do |occurrences, value|
-    printf("%2d: %2d\n", value, occurrences)
-    csv.write("#{value},#{occurrences}\n")
+    printf("%2d: %2d\n", value+2, occurrences)
+    csv.write("#{value+2},#{occurrences}\n")
 end
 
 # Graph it in R.
+csv.close
 `Rscript ./plot.r`
 File.delete("data.csv")
